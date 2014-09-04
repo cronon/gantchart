cec17fd5cc8b74b5df8b45135ef13eabe039c1c0
unknown <Dzmitry_Avdeev@EPBYMINW3273.minsk.epam.com>
2014-09-04 17:12:33 +0300

    edit styles

--- scripts/left-table.js
+++ scripts/left-table.js
@@ -6,6 +6,12 @@ $(function(){
         })
         .resizableColumns()
         .editableTableWidget()
+    $("#tasks th:first-child").css("width","1.49%");
+    $("#tasks th:eq(1)").css("width","31%");
+    $("#tasks th:eq(2)").css("width","14.7%");
+    $("#tasks th:eq(3)").css("width","13.2%");
+    $("#tasks th:eq(4)").css("width","11%");
+    $("#tasks th:eq(5)").css("width","6.6%");
 
     $("#tasks td:first-child").on('click', function(e){
       window.ev = e;
