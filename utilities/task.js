b25e7b4b9916fc6e2fd2a27b75b3755933baad88
Cronon <cronon12@gmail.com>
2014-09-09 23:44:44 +0300

    defined public properties for Task object

--- utilities/task.js
+++ utilities/task.js
@@ -1,29 +1,13 @@
 'use strict';
 
+/*
+
+*/
 var Task = function(options){
-  this.isNeedCall
-  this.name = options.name;
-  if (options.start !== undefined && options.end !== undefined){
-    this.startDate = options.start;
-    this.endDate = options.end;
-  } else if (options.start !== undefined && options.duration !== undefined){
-    this.startDate = options.start;
-    this.duration = options.duration;
-    this.endDate = getEndTask(this.startDate, this.duration);
-  } else if (options.timespan !== undefined && options.duration !== undefined){
-    // addNeighbour for this case
-    this.timespan = options.timespan;
-    this.duration = options.duration;
-  }
-
-  this.addNeighbour = function (neighbour) {
-    this.next = neighbour;
-    this.timespan = timespan;
-    neighbour.startDate = getEndTask(this.start, timespan);
-  };
-
-  this.setNext = function (next) {
-    this.next = next;
-  };
-
+  this.start = new Date();
+  this.end = new Date();
+  this.duration  // milliseconds
+  this.name = "new task";
+  this.prev = "21ОН - 4 дня"
+  this.subtasks = [new Task(), new Task()]
 }
